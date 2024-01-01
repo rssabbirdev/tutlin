2e22b1a547539fee0406d588d16edfa86a22c91e
rssabbirdev <sabbirhossainrahad@gmail.com>
2024-01-01 22:59:17 +0600

    Modify header

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -8,7 +8,6 @@ import { usePathname } from 'next/navigation'
 import { Header } from '../../../../payload/payload-types'
 import { noHeaderFooterUrls } from '../../../constants'
 import { Gutter } from '../../Gutter'
-import MobileNav from '../MobileNav'
 import { HeaderNav } from '../Nav'
 
 import classes from './index.module.scss'
@@ -21,12 +20,11 @@ function HeaderComponent({ header }: { header: Header }) {
         .filter(Boolean)
         .join(' ')}
     >
-      <Gutter className={[classes.wrap].filter(Boolean).join(' ')}>
+      <Gutter className={classes.wrap}>
         <Link href="/">
           <Image src="/logo-black.svg" alt="logo" height={50} width={170} />
         </Link>
         <HeaderNav header={header} />
-        <MobileNav header={header} />
       </Gutter>
     </nav>
   )
--- src/app/_components/Header/MobileNav/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function MobileNav({header}) {
-  return <div>MobileNav</div>
-}
-
-export default MobileNav
