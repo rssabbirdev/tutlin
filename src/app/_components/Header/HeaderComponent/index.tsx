83a77b266627008616ba13e92cc47aae57c11168
rssabbirdev <sabbirhossainrahad@gmail.com>
2024-01-01 22:48:27 +0600

    working with nav section

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,8 +1,12 @@
+'use client'
+
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 import { Header } from '../../../../payload/payload-types'
+import { noHeaderFooterUrls } from '../../../constants'
 import { Gutter } from '../../Gutter'
 import MobileNav from '../MobileNav'
 import { HeaderNav } from '../Nav'
@@ -10,9 +14,14 @@ import { HeaderNav } from '../Nav'
 import classes from './index.module.scss'
 
 function HeaderComponent({ header }: { header: Header }) {
+  const pathname = usePathname()
   return (
-    <nav className={[classes.header]}>
-      <Gutter className={[classes.wrap]}>
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      <Gutter className={[classes.wrap].filter(Boolean).join(' ')}>
         <Link href="/">
           <Image src="/logo-black.svg" alt="logo" height={50} width={170} />
         </Link>
--- src/app/_components/Header/MobileNav/index.tsx
+++ src/app/_components/Header/MobileNav/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function MobileNav() {
+function MobileNav({header}) {
   return <div>MobileNav</div>
 }
 
