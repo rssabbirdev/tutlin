74644c3499f501226dd0d85b36dc7e4bff882b75
rssabbirdev <sabbirhossainrahad@gmail.com>
2024-01-01 17:18:40 +0600

    add nav section

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
 import Link from 'next/link'
 
 import { Header } from '../../../../payload/payload-types'
 import { Gutter } from '../../Gutter'
+import MobileNav from '../MobileNav'
+import { HeaderNav } from '../Nav'
+
+import classes from './index.module.scss'
 
 function HeaderComponent({ header }: { header: Header }) {
   return (
-    <nav>
-      <Gutter>
+    <nav className={[classes.header]}>
+      <Gutter className={[classes.wrap]}>
         <Link href="/">
-          <img src="/logo-black.svg" alt="logo" />
+          <Image src="/logo-black.svg" alt="logo" height={50} width={170} />
         </Link>
+        <HeaderNav header={header} />
+        <MobileNav header={header} />
       </Gutter>
     </nav>
   )
