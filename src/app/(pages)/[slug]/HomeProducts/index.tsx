c5cb7ee1adc2310b3e7960fd06a4becb400bbdef
rssabbirdev <sabbirhossainrahad@gmail.com>
2024-01-12 21:45:20 +0400

    [ADDED] products display in home page

--- src/app/(pages)/[slug]/HomeProducts/index.tsx
+++ src/app/(pages)/[slug]/HomeProducts/index.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import Link from 'next/link'
 
 import { Product } from '../../../../payload/payload-types'
+import { Card } from '../../../_components/Card'
+
+import classes from './index.module.scss'
 
 function HomeProducts({ products }: { products: Product[] }) {
   return (
     <div>
-      {products.map(product => (
-        <h1>{product.title}</h1>
-      ))}
+      <div className={classes.titleWrapper}>
+        <h3>Explore Our Latest Products</h3>
+        <Link href="/products">See More</Link>
+      </div>
+      <div className={classes.products}>
+        {products.map(product => (
+          <Card
+            doc={product}
+            alignItems="center"
+            key={product.id}
+            title={product.title}
+            className={classes.product}
+          />
+        ))}
+      </div>
     </div>
   )
 }
