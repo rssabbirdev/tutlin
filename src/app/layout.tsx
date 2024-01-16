bcbac9235a97b54d2a46521784b8b8f00f90c631
rssabbirdev <sabbirhossainrahad@gmail.com>
2024-01-16 23:46:48 +0400

    give access to facebook and google in csp.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
+import { GoogleTagManager } from '@next/third-parties/google'
 import { Metadata } from 'next'
 import { Jost } from 'next/font/google'
-import Script from 'next/script'
 
 import { AdminBar } from './_components/AdminBar'
 import { Footer } from './_components/Footer'
@@ -24,31 +24,8 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <InitTheme />
         <link rel="icon" href="/favicon.ico" sizes="32x32" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        {/* <!-- Google Tag Manager --> */}
-        // eslint-disable-next-line @next/next/next-script-for-ga
-        <Script
-          id="google-tag-manager"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PRVSW5N3');
-            `,
-          }}
-        />
-        {/* <!-- End Google Tag Manager --> */}
       </head>
       <body className={jost.variable}>
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `
-              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PRVSW5N3"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>
-            `,
-          }}
-        />
         <Providers>
           <AdminBar />
           {/* @ts-expect-error */}
@@ -57,6 +34,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe>
           {/* @ts-expect-error */}
           <Footer />
         </Providers>
+        <GoogleTagManager gtmId="GTM-PRVSW5N3" />
       </body>
     </html>
   )
